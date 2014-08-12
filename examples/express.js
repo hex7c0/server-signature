ea108b9cf5c9ad11c4d87bc3336336e4f5da3e19
hex7c0 <carniellifrancesco@gmail.com>
2014-08-12 02:55:28 +0200

    examples

---
+++ examples/express.js
@@ -0,0 +1,32 @@
+"use strict";
+/**
+ * @file express example
+ * @module server-signature
+ * @package server-signature
+ * @subpackage examples
+ * @version 0.0.1
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+try {
+    var signature = require('../index.min.js'); // require('server-signature')
+    var app = require('express')();
+} catch (MODULE_NOT_FOUND) {
+    console.error(MODULE_NOT_FOUND);
+    process.exit(1);
+}
+
+app.use(signature());
+app.get('/', function(req, res) {
+
+    res.send('ok');
+    return;
+});
+// server-signature starting
+app.listen(3000);
+console.log('starting "hello world" on port 3000');
