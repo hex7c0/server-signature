d8095a0babc42994adc7fd5fd9f6120c575d9d2a
hex7c0 <carniellifrancesco@gmail.com>
2014-10-30 13:09:11 +0100

    standalone test

---
+++ test/standalone.js
@@ -0,0 +1,37 @@
+'use strict';
+/**
+ * @file standalone test
+ * @module server-signature
+ * @package server-signature
+ * @subpackage test
+ * @version 0.0.1
+ * @author https://github.com/expressjs/serve-static
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+try {
+    var signature = require('../index.min.js')({
+        standalone: true
+    });
+} catch (MODULE_NOT_FOUND) {
+    console.error(MODULE_NOT_FOUND);
+    process.exit(1);
+}
+
+/*
+ * test module
+ */
+describe('standalone', function() {
+
+    it('should return standard signature', function(done) {
+
+        var t = signature();
+        if (/^Nodejs\/0.1[0-1]{1,1}.[0-9]{1,2} \(/.test(t)) {
+            done();
+        }
+    });
+});
